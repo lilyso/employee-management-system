c4499b9292506c7ef4c64c67feb7768af9d881d9
lilys0 <lsouryaphanh@gmail.com>
2021-11-15 15:50:51 +1100

    readme update, error handling

--- README.md
+++ README.md
@@ -1,3 +1,107 @@
 # Employee Management System
 
-A command-line application to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+> A command-line application to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+> Demo [_here_](https://www.example.com).
+
+## Table of Contents
+
+- [General Info](#general-information)
+- [Technologies Used](#technologies-used)
+- [Dependencies](#dependencies)
+- [Features](#features)
+- [Screenshots](#screenshots)
+- [Setup](#setup)
+- [Usage](#usage)
+- [Room for Improvement](#room-for-improvement)
+- [Contact](#contact)
+- [License](#license)
+
+## General Information
+
+- Provide general information about your project here.
+- What problem does it (intend to) solve?
+- What is the purpose of your project?
+- Why did you undertake it?
+
+## Technologies Used
+
+- JavaScript
+- Node.js
+
+## Dependencies
+
+- inquirer.js
+- mysql2
+- colors.js
+- console.table
+- dotenv
+
+## Features
+
+- View, add, delete and update employees
+- View, add and delete employee roles
+- View, add and delete departments
+
+## Screenshots
+
+![Employee Management System Screenshot](src/assets/images/employee-database-screenshot.png)
+
+## Setup
+
+1. Git clone repository from the command line.
+
+```shell
+git clone https://github.com/lilyso/employee-management-system.git
+```
+
+2. Install dependecies by executing the following in your command line:
+
+```
+npm install
+```
+
+3. Set up your environment variables for your mysql database:
+
+![env-example screenshot](src/assets/images/env-example1.png)
+
+4. Set up your mysql database:
+
+```shell
+mysql -u root -p
+(* Use "root" or preferred user login)
+
+Enter Password:
+(* Your mysql password)
+
+source db/schema.sql
+
+source db/seeds.sql
+(* This is the test value file. Do not execute if not needed or enter your preferred values)
+```
+
+## Usage
+
+To start using the Employee Management System, enter:
+
+```
+node index.js
+```
+
+Once executed, the employee management options will be displayed.
+
+## Room for Improvement
+
+Room for improvement:
+
+- Ability to filter employees by manager.
+- Ability to update employees by manager.
+- Ability to view employees by department.
+- Ability to view the total utilised budget of a department—in other words, the combined salaries of all employees in that department.
+
+## Contact
+
+Created by [@lilyso](https://github.com/lilyso) - feel free to contact me!
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
--- src/manage.js
+++ src/manage.js
@@ -18,7 +18,6 @@ async function askQ(db) {
   inquirer
     .prompt(startQs)
     .then((answer) => {
-      console.log(answer.start);
       switch (answer.start) {
         case "View all Employees":
           selectEmployees(db);
@@ -59,7 +58,7 @@ async function askQ(db) {
       if (error.isTtyError) {
         console.log(colors.red("Your console environment is not supported!"));
       } else {
-        console.log(error);
+        console.log(colors.red(`An error has occurred - ${error}`));
       }
     });
 }
@@ -67,133 +66,189 @@ async function askQ(db) {
 // View all employees
 
 async function selectEmployees(db) {
-  let [allEmployees] = await db.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, employee_role.title, department.name AS department, employee_role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS Manager FROM employee employee LEFT JOIN employee manager ON employee.manager_id = manager.id INNER JOIN employee_role ON employee.role_id=employee_role.id INNER JOIN department ON employee_role.department_id=department.id ORDER By last_name"
-  );
-  console.table("\r", allEmployees);
-  askQ(db);
+  try {
+    let [allEmployees] = await db.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, employee_role.title, department.name AS department, employee_role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS Manager FROM employee employee LEFT JOIN employee manager ON employee.manager_id = manager.id INNER JOIN employee_role ON employee.role_id=employee_role.id INNER JOIN department ON employee_role.department_id=department.id ORDER By last_name"
+    );
+    console.table("\r", allEmployees);
+    askQ(db);
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured selecting employees - ${error}\n`)
+    );
+  }
 }
 
 // Add employees
 
 async function addEmployee(db) {
-  let [roles] = await db.query("SELECT id, title FROM employee_role");
-  let [managers] = await db.query(
-    "SELECT * FROM employee WHERE manager_id IS null"
-  );
-  let getRoleMan = addEmployeeQs({ roles, managers });
-  let employeeQs = await inquirer.prompt(getRoleMan).then(async (answer) => {
-    let addEmployee = await db.query(
-      `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.fname}", "${answer.lname}", ${answer.role}, ${answer.manager})`
+  try {
+    let [roles] = await db.query("SELECT id, title FROM employee_role");
+    let [managers] = await db.query(
+      "SELECT * FROM employee WHERE manager_id IS null"
     );
-    console.log(managers);
-    console.log(colors.green("\nEmployee has been added\n"));
-    askQ(db);
-  });
+    let getRoleMan = addEmployeeQs({ roles, managers });
+    let employeeQs = await inquirer.prompt(getRoleMan).then(async (answer) => {
+      let addEmployee = await db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.fname}", "${answer.lname}", ${answer.role}, ${answer.manager})`
+      );
+      console.log(colors.green("\nEmployee has been added\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured adding employee - ${error}\n`)
+    );
+  }
 }
-
 // Update employee role
 
 async function updateEmployeeRole(db) {
-  let [showEmployees] = await db.query("SELECT * FROM employee");
-  let [findNewRole] = await db.query("SELECT * FROM employee_role");
-  let updateRoleQs = updateEmpRoleQs(showEmployees, findNewRole);
-  let roleQs = await inquirer.prompt(updateRoleQs).then(async (answer) => {
-    let updateRole = await db.query(
-      `UPDATE employee SET employee.role_id = ${answer.newRole} WHERE employee.id = ${answer.employee}`
+  try {
+    let [showEmployees] = await db.query("SELECT * FROM employee");
+    let [findNewRole] = await db.query("SELECT * FROM employee_role");
+    let updateRoleQs = updateEmpRoleQs(showEmployees, findNewRole);
+    let roleQs = await inquirer.prompt(updateRoleQs).then(async (answer) => {
+      let updateRole = await db.query(
+        `UPDATE employee SET employee.role_id = ${answer.newRole} WHERE employee.id = ${answer.employee}`
+      );
+      console.log(colors.green("\nRole has been updated\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured updating employee role - ${error}\n`)
     );
-    console.log(answer);
-    console.log(colors.green("\nRole has been updated\n"));
-    askQ(db);
-  });
+  }
 }
 
 // Delete employee
 
 async function deleteEmployee(db) {
-  let [findEmployees] = await db.query("SELECT * FROM employee");
-  let delEmpQs = deleteEmployeeQs(findEmployees);
-  let roleQs = await inquirer.prompt(delEmpQs).then(async (answer) => {
-    let deleteEmpNow = await db.query(
-      `DELETE FROM employee WHERE ${answer.employees} = employee.id`
+  try {
+    let [findEmployees] = await db.query("SELECT * FROM employee");
+    let delEmpQs = deleteEmployeeQs(findEmployees);
+    let roleQs = await inquirer.prompt(delEmpQs).then(async (answer) => {
+      let deleteEmpNow = await db.query(
+        `DELETE FROM employee WHERE ${answer.employees} = employee.id`
+      );
+      console.log(colors.yellow("\nEmployee has been deleted\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured deleting employee - ${error}\n`)
     );
-    console.log(colors.yellow("\nEmployee has been deleted\n"));
-    askQ(db);
-  });
+  }
 }
 
 // View all roles
 
 async function viewRoles(db) {
-  let [allRoles] = await db.query(
-    "SELECT employee_role.id, employee_role.title,department.name, employee_role.salary FROM employee_role INNER JOIN department ON employee_role.department_id = department.id"
-  );
-  console.table("\r", allRoles);
-  askQ(db);
+  try {
+    let [allRoles] = await db.query(
+      "SELECT employee_role.id, employee_role.title,department.name, employee_role.salary FROM employee_role INNER JOIN department ON employee_role.department_id = department.id"
+    );
+    console.table("\r", allRoles);
+    askQ(db);
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured selecting employee roles - ${error}\n`)
+    );
+  }
 }
 
 // Add roles
 
 async function addRole(db) {
-  let [departments] = await db.query("SELECT * FROM department");
-  let qs = addRoleQs(departments);
-  let roleQs = await inquirer.prompt(qs).then(async (answer) => {
-    let addRole = await db.query(
-      `INSERT INTO employee_role (title, salary, department_id) VALUES ("${answer.title}", "${answer.salary}", ${answer.department})`
+  try {
+    let [departments] = await db.query("SELECT * FROM department");
+    let qs = addRoleQs(departments);
+    let roleQs = await inquirer.prompt(qs).then(async (answer) => {
+      let addRole = await db.query(
+        `INSERT INTO employee_role (title, salary, department_id) VALUES ("${answer.title}", "${answer.salary}", ${answer.department})`
+      );
+      console.log(colors.green("\nRole has been added\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured adding employee roles - ${error}\n`)
     );
-    console.log(answer);
-    console.log(colors.green("\nRole has been added\n"));
-    askQ(db);
-  });
+  }
 }
 
 // Delete Role
 
 async function deleteRole(db) {
-  let [findRoles] = await db.query("SELECT * FROM employee_role");
-  let eq = deleteRoleQs(findRoles);
-  let findRoleQs = await inquirer.prompt(eq).then(async (answer) => {
-    let deleteRoleNow = await db.query(
-      `DELETE FROM employee_role WHERE ${answer.roles} = employee_role.id`
+  try {
+    let [findRoles] = await db.query("SELECT * FROM employee_role");
+    let eq = deleteRoleQs(findRoles);
+    let findRoleQs = await inquirer.prompt(eq).then(async (answer) => {
+      let deleteRoleNow = await db.query(
+        `DELETE FROM employee_role WHERE ${answer.roles} = employee_role.id`
+      );
+      console.log(colors.yellow("\nRole has been deleted\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured deleting employee roles - ${error}\n`)
     );
-    console.log(colors.yellow("\nRole has been deleted\n"));
-    askQ(db);
-  });
+  }
 }
 
 // View all departments
 
 async function viewDep(db) {
-  let [allDep] = await db.query("SELECT * FROM department");
-  console.table("\r", allDep);
-  askQ(db);
+  try {
+    let [allDep] = await db.query("SELECT * FROM department");
+    console.table("\r", allDep);
+    askQ(db);
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occured selecting departments - ${error}\n`)
+    );
+  }
 }
 
 // Add department
 
 async function addDep(db) {
-  let [rows] = await db.query("SELECT * FROM department");
-  let addNewDepQs = await inquirer.prompt(addDepQs).then(async (answer) => {
-    let addNewDep = await db.query(
-      `INSERT INTO department (name) VALUES ("${answer.department}")`
+  try {
+    let [rows] = await db.query("SELECT * FROM department");
+    let addNewDepQs = await inquirer.prompt(addDepQs).then(async (answer) => {
+      let addNewDep = await db.query(
+        `INSERT INTO department (name) VALUES ("${answer.department}")`
+      );
+      console.log(colors.green("\nDepartment has been added\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occurred adding department - ${error}\n`)
     );
-    console.log(colors.green("\nDepartment has been added\n"));
-    askQ(db);
-  });
+  }
 }
 
 // Delete department
 
 async function deleteDep(db) {
-  let [departments] = await db.query("SELECT * FROM department");
-  let dq = deleteDepQs(departments);
-  let deleteDep = await inquirer.prompt(dq).then(async (answer) => {
-    let deleteDepNow = await db.query(
-      `DELETE FROM department WHERE department.id = ${answer.department}`
+  try {
+    let [departments] = await db.query("SELECT * FROM department");
+    let dq = deleteDepQs(departments);
+    let deleteDep = await inquirer.prompt(dq).then(async (answer) => {
+      let deleteDepNow = await db.query(
+        `DELETE FROM department WHERE department.id = ${answer.department}`
+      );
+      console.log(colors.yellow("\nDepartment has been deleted\n"));
+      askQ(db);
+    });
+  } catch (error) {
+    console.log(
+      colors.red(`\nAn error has occurred deleting department - ${error}\n`)
     );
-    console.log(colors.yello("\nDepartment has been deleted\n"));
-    askQ(db);
-  });
+  }
 }
 
 module.exports = askQ;
